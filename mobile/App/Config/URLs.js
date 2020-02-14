5a4a2077a17160727573657feab4ba1463fa4292
Stephanie Ding <sding@caltech.edu>
2020-02-14 02:03:21 -0800

    Initial commit of server and react native mobile app

---
+++ mobile/App/Config/URLs.js
@@ -0,0 +1,4 @@
+const BASE_URL = 'https://massive-cougar-86.localtunnel.me'
+const API_URL = '/api'
+export const LOGIN_URL = BASE_URL + '/oauth/token'
+export const USER_URL = BASE_URL + API_URL + '/user'
